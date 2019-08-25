7bcfae147d7e934093d6ce177a953af4bf50178b
Scott Yoon <scottyoon221@yahoo.com>
2019-08-25 12:25:36 -0700

    feat: complete cheopardy game!

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# cheopardy
-Quiz competition
+# Cheopardy!
+Quiz competition for brave contestants
+
+## Author
+Scott S. Yoon
+
+## Usage
+1. Fork/Clone this project
+2. Open topics.js file and Update topics, amounts, questions and answers
+3. Launch index.html file in browser
+
+## Development
+The game was built with HTML, CSS and vanilla JS. No library installation required.
+
+## How to play
+1. To play Cheopardy! music, click on sound button on the bottom right corner.
+![Sound](screenshot/sound.jpg?raw=true "sound")
+
+2. Select the amount of interest.
+![amount](screenshot/amount.jpg?raw=true "amount")
+
+3. Upon seeing the question, you have option to go back to Cheopardy! board by clicking on 'X' button on the top right corner. If you have answer, click 'Click here for answer'
+![question](screenshot/question.jpg?raw=true "question")
+
+4. Any answered question will be highlighted in red.
+![red](screenshot/red.jpg?raw=true "answered slot")
+
+
+
+
---
+++ topics.js
@@ -0,0 +1,147 @@
+const topicsArr = [
+  { "topic" : "HELLO NEWTON",
+    "questions" : [
+      {
+        "amount" : "$200",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      },
+      {
+        "amount" : "$300",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      },
+      {
+        "amount" : "$400",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      },
+      {
+        "amount" : "$500",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      },
+      {
+        "amount" : "$600",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      }
+    ]
+  },
+  { "topic" : "19TH CENTURY",
+    "questions" : [
+      {
+        "amount" : "$200",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      },
+      {
+        "amount" : "$300",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      },
+      {
+        "amount" : "$400",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      },
+      {
+        "amount" : "$500",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      },
+      {
+        "amount" : "$600",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      }
+    ]
+  },
+  { "topic" : "BUSINESS LETTERS",
+    "questions" : [
+      {
+        "amount" : "$200",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      },
+      {
+        "amount" : "$300",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      },
+      {
+        "amount" : "$400",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      },
+      {
+        "amount" : "$500",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      },
+      {
+        "amount" : "$600",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      }
+    ]
+  },
+  { "topic" : "MATH",
+    "questions" : [
+      {
+        "amount" : "$200",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      },
+      {
+        "amount" : "$300",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      },
+      {
+        "amount" : "$400",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      },
+      {
+        "amount" : "$500",
+        "question" : "what is scott's last name?",
+        "answer" : "Yoon"
+      },
+      {
+        "amount" : "$600",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      }
+    ]
+  },
+  { "topic" : "JAVASCRIPT",
+    "questions" : [
+      {
+        "amount" : "$200",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      },
+      {
+        "amount" : "$300",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      },
+      {
+        "amount" : "$400",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      },
+      {
+        "amount" : "$500",
+        "question" : "What is.... the question?",
+        "answer" : "What is ... the answer."
+      },
+      {
+        "amount" : "$600",
+        "question" : "Test Q?",
+        "answer" : "Test Ans."
+      }
+    ]
+  }
+]
